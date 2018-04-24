935c5bada8ed23f57c6a06d247dddc4f3970be5e
杨世川 <yscbbc@gmail.com>
2018-04-24 09:47:15 +0800

    消息转发

--- 引用计数/MessageForwardingController.m
+++ 引用计数/MessageForwardingController.m
@@ -21,6 +21,14 @@
 
 }
 
+//总结:
+//   1、某个对象调用某个方法,发现自己没有实现,父类也没有实现,此时会进入消息转发机制
+//      此时系统会调用以下方法:
+//      a、+ (BOOL)resolveInstanceMethod:(SEL)selector//实例对象
+//         + (BOOL)resolveClassMethod:(SEL)selector//类对象
+//      b、- (id)forwardingTargetForSelector:(SEL)selector
+//         - (void)forwardInvocation:(NSInvocation *)invocation
+
 
 //参考文章:
 //1、深入理解Objective-C的Runtime机制
